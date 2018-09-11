4424bbe8f6afd7019e84b84c985fcdb9c094b207
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-11 21:20:31 +0700

    Update header

--- src/headers/teacrypt_php_function.h
+++ src/headers/teacrypt_php_function.h
@@ -2,7 +2,7 @@
 #ifndef __TEACRYPT_PHP_FUNCTION_H
 #define __TEACRYPT_PHP_FUNCTION_H 1
 
-Php::Value teaEncrypt(Php::Parameters &params);
-Php::Value teaDecrypt(Php::Parameters &params);
+Php::Value teaEncrypt(Php::Parameters);
+Php::Value teaDecrypt(Php::Parameters);
 
 #endif
